2c65f629c6b449573fc9643568162d218f34fa68
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2021-01-20 11:21:55 +0100

    Update Personnage.h

--- Personnage.h
+++ Personnage.h
@@ -1,32 +1,61 @@
-#include <iostream>
+#ifndef PERSONNAGE_H
+#define PERSONNAGE_H
+
 #include <string>
+#include <iostream>
+#include <random>
 
 using namespace std;
 
 class Personnage
 {
 private:
-    int pv= 100;
-    string nom = "Bob";
-    int attack = 10;
-    int defense = 10;
-    float percentCrit = 0,1f;
+	int pv = 100;
+	string nom = "Bob";
+	int attack = 10;
+	int defense = 10;
+	float percentCrit = 0.1f;
+	int heal = 10;
 
 public:
-    int getPv();
-    void setPv(int s);
+    //CONSTRUCTEURS
+    Personnage();
+    Personnage(int p, int a, float pc);
+    Personnage(string n);
 
-    string getNom();
-    void setNom();
-    
-    int getAttack();
-    void setAttack(int a);
 
-    int getDefense();
-    void setDefense(int d);
+    //DESTRUCTEUR
+    ~Personnage();
 
-    float getPercentCrit();
-    void setPercentCrit(int c);
+    //GETTER/SETTER PV
+	int getPv();
+	void setPv(int s);
 
-    void showInfos();
+    //GETTER/SETTER NOM
+	string getNom();
+	void setNom(string n);
+
+    //GETTER/SETTER HEAL
+	int getHeal();
+	void setHeal(int h);
+
+    //GETTER/SETTER ATTACK
+	int getAttack();
+	void setAttack(int a);
+
+    //GETTER/SETTER DEFENSE
+	int getDefense();
+	void setDefense(int d);
+
+    //GETTER/SETTER PERCENT CRIT
+	float getPercentCrit();
+	void setPercentCrit(float d);
+
+    //AFFICHE LES INFOS DU PERSO
+	void showInfos();
+
+    void attackPlayer(Personnage* cible);
+    void healPlayer(Personnage* cible);
 };
+
+#endif // PERSONNAGE_H
