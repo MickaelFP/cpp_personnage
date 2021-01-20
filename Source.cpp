f4b668fb32c2428f47c55fb40fb566dce132034e
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2021-01-20 11:18:30 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,21 +1,49 @@
 #include "Personnage.h"
 
 int main() {
-    string test;
-    Personnage p1;
-    p1.setPv(1000);
-    p1.setAttack(100);
-    p1.setDefense(100);
-    p1.setPercentCrit(0;2);
-    cout << "Pv du personnage 1 : " << p1.pv << endl;
-    p1.showInfos();
 
-    Personnage p2;
-    p2.setPv(500);
-    p2.setAttack(50);
-    p2.setDefense(50);
-    p2.setPercentCrit(0.5);
-    cout << "Pv du personnage 2 : " << p2.pv << endl;
-    p2.showInfos();
+	Personnage p1;
+	p1.setNom("Jean");
+	p1.setAttack(50);
+	p1.setDefense(20);
+	p1.setPercentCrit(0.2);
+	p1.showInfos();
+	cout << endl;
+
+	cout << "	personnage 2  " << endl;
+	Personnage p2(p1);
+	p2.showInfos();
+	cout << endl;
+
+	p1.~Personnage();
+
+	cout << "	personnage 3  " << endl;
+	Personnage p3(200,50,0.2);
+    p3.showInfos();
+	cout << endl;
+
+	cout << "	personnage 4 " << endl;
+	Personnage p4("Romain");
+    p4.showInfos();
+	cout << endl;
+
+	cout << "Attack de p4 = "<<p4.getAttack()<<endl;
+	cout << "PV de P3 = "<<p3.getPv()<<endl;
+
+    cout <<"ATTACK"<<endl;
+	p4.attackPlayer(&p3);
+
+	cout << "Attack de p4 = "<<p4.getAttack()<<endl;
+	cout << "PV de P3 = "<<p3.getPv()<<endl;
+
+	cout << "heal de p2 = "<<p2.getHeal()<<endl;
+	cout << "PV de P3 = "<<p3.getPv()<<endl;
+
+    cout <<"HEAL"<<endl;
+	p2.healPlayer(&p3);
+
+	cout << "heal de p2 = "<<p2.getHeal()<<endl;
+	cout << "PV de P3 = "<<p3.getPv()<<endl;
+
     return 0;
 }
